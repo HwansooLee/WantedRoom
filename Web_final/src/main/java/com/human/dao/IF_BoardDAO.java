34ab570c41e350fed2ec07e6368cbd779a8cf09a
HansLee <hwansu29@naver.com>
2023-06-29 17:20:15 +0900

    modified sql tables, added documents for dev

--- Web_final/src/main/java/com/human/dao/IF_BoardDAO.java
+++ Web_final/src/main/java/com/human/dao/IF_BoardDAO.java
@@ -5,6 +5,7 @@ import com.human.VO.BoardVO;
 import java.util.List;
 
 public interface IF_BoardDAO {
+    // page
     public void insert(BoardVO vo);
     public BoardVO selectOne(int boardNo);
     public List<BoardVO> selectAll(String searchWord);
